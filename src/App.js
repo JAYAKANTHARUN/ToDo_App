bafd22b7f23870afd3cec886426f66c1fbb4b1c8
Jayakanth Arun <jayakantharun03@gmail.com>
2023-03-26 19:03:44 +0530

    Added progress bar

--- src/App.js
+++ src/App.js
@@ -5,9 +5,23 @@ import { useState } from 'react';
 function App() {
   const [toDos, setToDos] = useState([])
   const [toDo, setToDo] = useState('')
+  const [active,setactive]=useState(0.0)
+  const [comp,setcomp]=useState(0.0)
 
-  const removeToDo = (id) => {
-    setToDos(toDos.filter((object) => object.id !== id))
+  var width = (comp/active)*100;
+  var intervalId = setInterval(function() {
+    if (width >= 100) {
+      clearInterval(intervalId);
+    }
+  }, 10);
+
+  const removeToDo = (obj) => {
+    setactive(active-1)
+    if (obj.status===true){
+      setcomp(comp-1)
+    }
+    
+    setToDos(toDos.filter((object) => object.id !== obj.id))
   }
 
   return (
@@ -21,11 +35,18 @@ function App() {
         <h2>"A goal without a plan is just a wish"</h2>
         <h2 className="name"> - Antoine de Saint-Exupéry</h2>
         <br /><br />
+        <div class="progress-bar">
+          <div class="progress-bar-fill" style={{ width: `${width}%` }}></div>
+        </div>
+        <br /><br />
         <h2>Add Today's Goals &#9917;</h2>
       </div>
       <div className="input">
         <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
+        <i onClick={() => {
+          setToDos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+          setactive(active+1);
+        }} className="fas fa-plus"></i>
       </div>
       <div className="todos">
         {toDos.map((obj) => {
@@ -35,6 +56,12 @@ function App() {
                 <input
                   checked={obj.status}
                   onChange={(e) => {
+                    if (obj.status===true){
+                      setcomp(comp-1)
+                    }
+                    if (obj.status===false){
+                      setcomp(comp+1)
+                    }
                     const updatedToDos = toDos.map((toDo) => {
                       if (toDo.id === obj.id) {
                         return {
@@ -51,7 +78,7 @@ function App() {
                 <p>{obj.text}</p>
               </div>
               <div className="right">
-                <i onClick={() => removeToDo(obj.id)} className="fas fa-times"></i>
+                <i onClick={() => removeToDo(obj)} className="fas fa-times"></i>
               </div>
             </div>
           )
